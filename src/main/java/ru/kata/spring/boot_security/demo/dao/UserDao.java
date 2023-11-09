6ee7ca786fc5ef8ca79b8b9afb8a8228b8a5f6f5
Ruslan Garmanov <garmanov.rus@mail.ru>
2023-11-09 03:04:18 +0300

    commit

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -1,10 +1,11 @@
 package ru.kata.spring.boot_security.demo.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
-public interface UserDao {
+public interface UserDao extends JpaRepository<User, Long> {
     void saveUser(User user);
 
     void updateUser(User user);
